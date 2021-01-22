541b9e5a57e2e41508ce906e0b0607ba59a4e3f2
Offliner <42777325+Offliners@users.noreply.github.com>
2021-01-23 02:19:30 +0800

    Create 2_1.cpp

---
+++ Chapter2/2_1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cout << "Name : Offliner" << endl;
+	cout << "Address : No. 1, Sec. 4, Roosevelt Road, Taipei, 106319 Taiwan (R.O.C.)" << endl;
+
+	return 0;
+}
