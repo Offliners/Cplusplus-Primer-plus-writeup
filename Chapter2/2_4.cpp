12f75880524a464a79b17b54ae652af90d514f3f
Offliner <42777325+Offliners@users.noreply.github.com>
2021-01-23 02:32:59 +0800

    Create 2_4.cpp

---
+++ Chapter2/2_4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int age, mon;
+
+	cout << "Enter your age: ";
+	cin >> age;
+
+	mon = age * 12;
+
+	cout << "Your age in months is " << mon << endl;
+
+	return 0;
+}
