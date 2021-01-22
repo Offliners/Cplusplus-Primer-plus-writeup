3774950d104275c4374b89052729e5e4054d9982
Offliner <42777325+Offliners@users.noreply.github.com>
2021-01-23 02:43:32 +0800

    Create 2_7.cpp

---
+++ Chapter2/2_7.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int hr, min;
+
+	cout << "Enter the number of hours: ";
+	cin >> hr;
+
+	cout << "Enter the number of minutes: ";
+	cin >> min;
+
+	cout << "Time: " << hr << ":" << min << endl;
+
+	return 0;
+}
