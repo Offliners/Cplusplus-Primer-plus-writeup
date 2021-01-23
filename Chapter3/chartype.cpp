b31d42f26215f46da23ff2886a371887030e53b0
Offliner <42777325+Offliners@users.noreply.github.com>
2021-01-23 23:01:40 +0800

    Create chartype.cpp

---
+++ Chapter3/chartype.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	char ch;
+
+	cout << "Enter a character: " << endl;
+	cin >> ch;
+
+	cout << "Holla! ";
+	cout << "Thank you for the " << ch << " character." << endl;
+
+	return 0;
+}
