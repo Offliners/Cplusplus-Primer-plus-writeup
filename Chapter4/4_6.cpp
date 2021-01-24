a3dde08c36d444d022fa800261d94c573b7b2cd7
Offliner <42777325+Offliners@users.noreply.github.com>
2021-01-24 21:23:47 +0800

    Create 4_6.cpp

---
+++ Chapter4/4_6.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+struct CandyBar
+{
+	string brand;
+	double weight;
+	int cal;
+};
+
+int main(void)
+{
+	CandyBar snack[3] =
+	{
+		{"Mocha Munch", 2.3, 350},
+		{"Mocha Munch", 2.3, 350},
+		{"Mocha Munch", 2.3, 350}
+	};
+
+	cout << "Snack 1 :" << endl;
+	cout << "Snack name : " << snack[0].brand << endl;
+	cout << "Snack weight : " << snack[0].weight << endl;
+	cout << "Snack calories : " << snack[0].cal << endl;
+	cout << endl;
+
+	cout << "Snack 2 :" << endl;
+	cout << "Snack name : " << snack[1].brand << endl;
+	cout << "Snack weight : " << snack[1].weight << endl;
+	cout << "Snack calories : " << snack[1].cal << endl;
+	cout << endl;
+
+	cout << "Snack 3 :" << endl;
+	cout << "Snack name : " << snack[2].brand << endl;
+	cout << "Snack weight : " << snack[2].weight << endl;
+	cout << "Snack calories : " << snack[2].cal << endl;
+	cout << endl;
+
+	return 0;
+}
