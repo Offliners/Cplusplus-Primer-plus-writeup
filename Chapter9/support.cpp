65d234c5433cd324ba4a75f6a0b069d18f4477ff
Offliner <42777325+Offliners@users.noreply.github.com>
2021-01-25 16:54:04 +0800

    Create support.cpp

---
+++ Chapter9/support.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+extern double warming;
+
+void update(double dt);
+void local(void);
+
+void update(double dt)
+{
+	extern double warming;
+
+	warming += dt;
+	cout << "Updating global warming to " << warming;
+	cout << " degrees.\n";
+}
+
+void local()
+{
+	double warming = 0.8;
+
+	cout << "Local warming to " << warming << " degrees.\n";
+	cout << "But global warming = " << ::warming;
+	cout << " degrees.\n";
+}
