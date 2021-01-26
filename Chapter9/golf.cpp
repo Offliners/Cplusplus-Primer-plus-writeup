0e8db602d543220e7083343386da7aa15f8a5c26
Offliner <42777325+Offliners@users.noreply.github.com>
2021-01-26 18:35:35 +0800

    Create golf.cpp

---
+++ Chapter9/golf.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include"golf.h"
+using namespace std;
+
+void setgolf(golf& g, const char* name, int hc)
+{
+	strcpy(g.fullname, name);
+	g.handicap = hc;
+}
+
+int setgolf(golf& g)
+{
+	int count;
+	char* pc = new char[Len];
+
+	cout << "Enter the name of PC :\n";
+	cin.getline(pc, Len);
+
+	if (pc[0] == '\0' || cin.eof())
+		return 0;
+	else
+	{
+		cout << "Enter the handicap : ";
+		cin >> count;
+		cin.get();
+
+		setgolf(g, pc, count);
+		delete[] pc;
+
+		return 1;
+	}
+}
+
+void handicap(golf& g, int hc)
+{
+	g.handicap = hc;
+}
+
+void showgolf(const golf& g)
+{
+	cout << "Name : " << g.fullname << endl;
+	cout << "Handicap : " << g.handicap << endl;
+}
