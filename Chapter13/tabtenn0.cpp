a314d008992fdd3b38479f9c144417c980f4c3a6
Offliner <42777325+Offliners@users.noreply.github.com>
2021-01-28 14:54:38 +0800

    Create tabtenn0.cpp

---
+++ Chapter13/tabtenn0.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"tabtenn0.h"
+
+using namespace std;
+
+TableTennisPlayer::TableTennisPlayer(const string& fn, const string& ln, bool ht) : firstname(fn), lastname(ln), hasTable(ht) {};
+
+void TableTennisPlayer::Name() const
+{
+	cout << lastname << ", " << firstname;
+}
