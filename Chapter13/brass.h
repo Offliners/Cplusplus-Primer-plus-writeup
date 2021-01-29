0eaeef9acfae54661470c86dec91e96424ebd88e
Offliner <42777325+Offliners@users.noreply.github.com>
2021-01-29 09:30:27 +0800

    Update brass.h

--- Chapter13/brass.h
+++ Chapter13/brass.h
@@ -28,8 +28,8 @@ class BrassPlus : public Brass
 		double rate;
 		double owesBank;
 	public:
-		BrassPlus(const string& s = "Nullbody", loan an = -1, double bal = 0.0, double ml = 500, double r = 0.11125);
-		BrassPlue(const Brass& ba, double ml = 500, double r = 0.11125);
+		BrassPlus(const string& s = "Nullbody", long an = -1, double bal = 0.0, double ml = 500, double r = 0.11125);
+		BrassPlus(const Brass& ba, double ml = 500, double r = 0.11125);
 
 		virtual void ViewAcct() const;
 		virtual void Withdraw(double amt);
