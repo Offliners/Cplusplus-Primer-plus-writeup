b3e423c6f7df5a5f5dc4f99ed9bd54fbb82f684c
Offliner <42777325+Offliners@users.noreply.github.com>
2021-01-29 22:29:21 +0800

    Create 13_1.cpp

---
+++ Chapter13/13_1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include"classic.h"
+
+using namespace std;
+
+void Bravo(const Cd& disk);
+
+int main(void)
+{
+	Cd c1("Beatles", "Capitol", 14, 35.5);
+	Classic c2 = Classic("Pinao Sonata in B flat, Fantasia in C", "Alfred Brendel", "Philips", 2, 57.17);
+	
+	Cd* pcd = &c1;
+
+	cout << "Using object directly : \n";
+	c1.Report();
+	c2.Report();
+
+	cout << "Using type cd* pointer to objects : \n";
+	pcd->Report();
+	pcd = &c2;
+	pcd->Report();
+
+	cout << "Calling a function with a Cd reference argument :\n";
+	Bravo(c1);
+	cout << "For Classic class :\n";
+	Bravo(c2);
+
+	cout << "Testing assignment: ";
+	Classic copy;
+	copy = c2;
+	copy.Report();
+
+	return 0;
+}
+
+void Bravo(const Cd& disk)
+{
+	disk.Report();
+}
