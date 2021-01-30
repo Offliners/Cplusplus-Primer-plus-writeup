28a38d25a7cd829b3c7608c71af96f734b72e933
Offliner <42777325+Offliners@users.noreply.github.com>
2021-01-31 02:24:43 +0800

    Create waiting.cpp

---
+++ Chapter5/waiting.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<ctime>
+
+using namespace std;
+
+int main(void)
+{
+	float secs;
+
+	cout << "Enter the delay time, in seconds: ";
+	cin >> secs;
+
+	clock_t delay = secs * CLOCKS_PER_SEC;
+	cout << "starting\a\n";
+	clock_t start = clock();
+
+	while (clock() - start < delay);
+	cout << "done \a\n";
+
+	return 0;
+}
