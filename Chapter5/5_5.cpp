1265d1aa77b86c96bdd103780326629a8be1dd6f
Offliner <42777325+Offliners@users.noreply.github.com>
2021-01-31 11:39:29 +0800

    Create 5_5.cpp

---
+++ Chapter5/5_5.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+const int n_months = 12;
+
+int main(void)
+{
+	string months[n_months] = { "January", "February", "March", "April", "March", "June", "July", "August", "September", "October",  "November", "December" };
+	int sales[n_months];
+	int sum = 0;
+
+	for (int i = 0; i < n_months; ++i)
+	{
+		cout << "Enter sales in " << months[i] << ": ";
+		
+		cin >> sales[i];
+		sum += sales[i];
+	}
+
+	cout << "Total sales in this year is " << sum << endl;
+
+	return 0;
+}
