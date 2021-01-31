6ebc3f160e6e45c41c9444c31feeea9e986aedf5
Offliner <42777325+Offliners@users.noreply.github.com>
2021-02-01 00:45:05 +0800

    Create error1.cpp

---
+++ Chapter15/error1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+double hmean(double a, double b);
+
+int main(void)
+{
+	double x, y, z;
+
+	cout << "Enter two numbers: ";
+	while (cin >> x >> y)
+	{
+		z = hmean(x, y);
+		cout << "Harmonic mean of " << x << " and " << y << " is " << z << endl;
+		cout << "Enter next set of numbers <q to quit>: ";
+	}
+
+	cout << "Bye!\n";
+
+	return 0;
+}
+
+double hmean(double a, double b)
+{
+	if (a == -b)
+	{
+		cout << "untenable arguments to hmean()\n";
+		abort();
+	}
+
+	return 2.0 * a * b / (a + b);
+}
